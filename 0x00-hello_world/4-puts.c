3d1dd70519dbe866901d12cce3186c70e219d461
mathias-tad <mathiasstadesse@gmail.com>
2021-06-17 00:25:38 -0700

    Printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *print the sentence
- *returns 0
+ *main: print the sentence
+ *return: returns 0
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
