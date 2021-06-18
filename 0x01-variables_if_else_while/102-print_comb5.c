cf180bff617b8e2227d6ba9a678d74af85097217
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 12:41:56 -0700

    Print 4 combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ *main - starting of the code
+ *Return: always 0
+ */
+
+int main(void)
+{
+int num1 = '0';
+int num2 = '0';
+int num3 = '0';
+int num4 = '1';
+
+while (num1 <= '9')
+{
+while (num2 <= '9')
+{
+while (num3 <= '9')
+{
+while (num4 <= '9')
+{
+putchar(num1);
+putchar(num2);
+putchar(' ');
+putchar(num3);
+putchar(num4);
+
+if (num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9')
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+num4++;
+}
+num3++;
+num4 = 0;
+}
+num2++;
+num3 = 0;
+num4 = 1;
+}
+num1++;
+num2 = 0;
+num3 = 0;
+num4 = 1;
+}
+putchar('\n');
+
+return (0);
+}
