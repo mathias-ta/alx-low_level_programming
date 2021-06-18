41ffc1cadcd26276326ecefe9bf35a3c0499c30c
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 09:17:50 -0700

    Printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - starting of the code
+ *Return: always 0
+ */
+
+int main(void)
+{
+char alphabet = 'a';
+
+while (alphabet <= 'z'){
+
+putchar(alphabet);
+alphabet++;
+
+}
+putchar("\n");
+
+return (0);
+
+}
