f31c17cd43eaf9b1475dab7b036227a153fe1f4c
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 10:02:50 -0700

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - starting of the code
+ *Return: always 0
+ */
+
+int main(void)
+{
+char alphabet = 'Z';
+
+while (alphabet >= 'a')
+{
+
+putchar(alphabet);
+alphabet--;
+
+}
+putchar('\n');
+
+return (0);
+
+}
