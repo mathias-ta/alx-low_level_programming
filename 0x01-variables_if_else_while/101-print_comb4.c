2e8f6a5c0922b412fed029dc95b341bf8a28dd5d
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 12:04:30 -0700

    Print a combination of three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,44 +8,37 @@
 int main(void)
 {
 int singleDig_nums = '0';
-int singleDig_nums_2 = '1';
-int singleDig_nums_3 = '2';
+int singleDig_nums2 = '1';
+int singleDig_nums3 = '2';
 
 while (singleDig_nums <= '9')
 {
-
-while (singleDig_nums_2 <= '9')
+while (singleDig_nums2 <= '9')
 {
-
-while (singleDig_nums_3 <= '9')
+while (singleDig_nums3 <= '9')
 {
-
 putchar(singleDig_nums);
-putchar(singleDig_nums_2);
-putchar(singleDig_nums_3);
+putchar(singleDig_nums2);
+putchar(singleDig_nums3);
 
-if (singleDig_nums == '7' && singleDig_nums_2 == '8' && singleDig_nums_3 == '9')
+if (singleDig_nums == '7' && singleDig_nums2 == '8' && singleDig_num_3 == '9')
 {
 }
-
 else
 {
 putchar(',');
 putchar(' ');
 }
-singleDig_nums_3++;
+singleDig_nums3++;
 }
-singleDig_nums_2++;
-singleDig_nums_3 = singleDig_nums_2 + 1;
+singleDig_nums2++;
+singleDig_nums3 = singleDig_nums2 + 1;
 }
-
 singleDig_nums++;
-singleDig_nums_2 = singleDig_nums + 1;
- singleDig_nums_3 = singleDig_nums + 2;
-
+singleDig_nums2 = singleDig_nums + 1;
+singleDig_nums3 = singleDig_nums + 2;
 }
 putchar('\n');
 
 return (0);
-
 }
