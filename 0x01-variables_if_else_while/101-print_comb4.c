7caed48ae714af80087aa2a45a559c82a28810e5
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 11:58:53 -0700

    Print a combination of three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,24 +7,24 @@
 
 int main(void)
 {
-int single_digit_nums = '0';
-int single_digit_nums_2 = '1';
-int single_digit_nums_3 = '2';
+int singleDig_nums = '0';
+int singleDig_nums_2 = '1';
+int singleDig_nums_3 = '2';
 
-while (single_digit_nums <= '9')
+while (singleDig_nums <= '9')
 {
 
-while (single_digit_nums_2 <= '9')
+while (singleDig_nums_2 <= '9')
 {
 
-while (single_digit_nums_3 <= '9')
+while (singleDig_nums_3 <= '9')
 {
 
-putchar(single_digit_nums);
-putchar(single_digit_nums_2);
-putchar(single_digit_nums_3);
+putchar(singleDig_nums);
+putchar(singleDig_nums_2);
+putchar(singleDig_nums_3);
 
-if (single_digit_nums == '7' && single_digit_nums_2 == '8' && single_digit_nums_3 == '9')
+if (singleDig_nums == '7' && singleDig_nums_2 == '8' && singleDig_nums_3 == '9')
 {
 }
 
@@ -33,14 +33,15 @@ else
 putchar(',');
 putchar(' ');
 }
-single_digit_nums_3++;
+singleDig_nums_3++;
 }
-single_digit_nums_2++;
-single_digit_nums_3 = single_digit_nums_2 + 1;
+singleDig_nums_2++;
+singleDig_nums_3 = singleDig_nums_2 + 1;
 }
 
-single_digit_nums++;
-single_digit_nums_2 = single_digit_nums + 1;
+singleDig_nums++;
+singleDig_nums_2 = singleDig_nums + 1;
+ singleDig_nums_3 = singleDig_nums + 2;
 
 }
 putchar('\n');
