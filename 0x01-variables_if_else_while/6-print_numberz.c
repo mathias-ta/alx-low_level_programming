db6bbc1b929d7d2d556674a3691424376bafcd07
mathias-tad <mathiasstadesse@gmail.com>
2021-06-18 09:57:29 -0700

    Print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - starting of the code
+ *Return: always 0
+ */
+
+int main(void)
+{
+int single_digit_nums = '0';
+
+while (single_digit_nums <= '9')
+{
+
+putchar(single_digit_nums);
+single_digit_nums++;
+
+}
+putchar('\n');
+
+return (0);
+
+}
