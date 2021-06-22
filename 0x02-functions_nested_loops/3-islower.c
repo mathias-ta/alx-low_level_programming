9fa144611067777f339ee1a344621a2f36d97cb6
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 04:06:02 -0700

    Check lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- *islower - checks whether the character is lower or upper case
+ *_islower - checks whether the character is lower or upper case
  *Return: always 0
  */
 
-int islower(int c)
+int _islower(int c)
 {
 
 if (c >= 'a' && c <= 'z')
