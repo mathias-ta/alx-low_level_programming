da8fc61cb44eb3078769d8ae8701ecf24cef6be4
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 05:02:45 -0700

    Absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_abs - absolute value of a number
+ *Return: returns 0
+ */
+
+int _abs(int)
+{
+int num;
+if (num > 0)
+{
+return (num);
+
+}
+else if (n == 0)
+{
+return (0);
+
+}
+else
+{
+return (-1*num);
+
+}
+
+
+}
