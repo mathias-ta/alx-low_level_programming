b1e2f5eb927037fc846ef94f50d6cb8f4020015b
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 04:00:53 -0700

    Check lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *islower - checks whether the character is lower or upper case
+ *Return: always 0
+ */
+
+int islower(int c)
+{
+char charc;
+if (charc >= 'a' && charc <= 'z')
+{
+
+return (1);
+}
+else
+return (0);
+
+
+}
