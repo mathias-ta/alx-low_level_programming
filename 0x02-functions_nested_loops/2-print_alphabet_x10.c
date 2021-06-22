311aac0f961e5514632d631fcdad117ef6030ed5
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 03:45:37 -0700

    Print lowercases 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_alphabet_x10 - prints lowercase alphabets 10x
+ *Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+for (int i = 1; i <= 10; i++)
+{
+char alpha_small = 'a';
+while (alpha_small <= 'z')
+{
+_putchar(alpha_small);
+alpha_small++;
+}
+_putchar('\n');
+}
+
+
+}
