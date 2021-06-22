e49bb1b03b298b9bd896ba757483b790a982c741
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 04:42:27 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,20 @@ int print_sign(int n)
 if (n > 0)
 {
 
-return (1);
 putchar('+');
+return (1);
 }
 else if (n == 0)
 {
-return (0);
 putchar('0');
+return (0);
+
 }
 else
 {
-return (-1);
 putchar('-');
+return (-1);
+
 }
 
 
