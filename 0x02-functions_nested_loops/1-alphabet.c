a7745dc488f9293f014b7342bad9c47a9fa30dfd
mathias-tad <mathiasstadesse@gmail.com>
2021-06-22 03:26:34 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - starting of the code
+ *Return: always 0
+ */
+
+void print_alphabet(void)
+{
+char alpha_small = 'a';
+while (alpha_small >= 'a')
+{
+_putchar(alpha_small);
+}
+_putchar('\n');
+
+return (0);
+
+}
