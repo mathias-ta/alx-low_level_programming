5fa4f3914603ceea7bda95d904803305324dce10
mathias-tad <mathiasstadesse@gmail.com>
2021-06-24 06:21:11 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,27 @@ while (i <= 100)
 {
 if ((i % 3) == 0 && (i % 5) == 0)
 {
-printf("%cFizzBuzz ");
+printf("FizzBuzz");
 }
 else if ((i % 3) == 0)
 {
-printf("%cFizz ");
+printf("Fizz");
 }
 else if ((i % 5) == 0)
 {
-printf("%cBuzz");
+printf("Buzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (i < 100)
+{
+printf(" ");
 }
 i++;
 }
- printf("%d\n");
+printf("\n");
 
+return (0);
 }
