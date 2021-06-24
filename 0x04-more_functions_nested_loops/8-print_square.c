c59ff14e4602f53e1131ea0da64b9219b2f5949e
mathias-tad <mathiasstadesse@gmail.com>
2021-06-24 05:45:38 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_diagonal - print space diagonaly
+ *@size: size of the square
+ *Return: void
+ */
+
+void print_square(int size)
+{
+int i = 1;
+int j;
+
+if (n > 0)
+{
+while (i <= n)
+{
+
+for (j = 1; j <= n; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+i++;
+}
+}
+else
+{
+_putchar('\n');
+}
+
+
+}
