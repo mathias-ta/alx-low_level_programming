9a1b934380e26226ad2ef776643e20ab86703ba6
mathias-tad <mathiasstadesse@gmail.com>
2021-06-24 06:44:46 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_triangle - print a triangle
+ *@size: size of the triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+int i = 1;
+int j = size - 1;
+
+if (size > 0)
+{
+while (i <= size)
+{
+
+while (j >= 1)
+{
+_putchar(' ');
+j--;
+}
+j++;
+while (k < size - 1)
+{
+_putchar('#');
+}
+k = j;
+_putchar('\n');
+i++;
+}
+}
+else
+{
+_putchar('\n');
+}
+
+
+}
