8bfafca4fa5c6046c4350b0cc475f6f891d6e9ec
mathias-tad <mathiasstadesse@gmail.com>
2021-06-24 04:25:16 -0700

    Print 1 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *more_numbers - print 0 to 14
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+int i = '0';
+int j, k;
+for (k = 0; k < 10; k++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (i > '9')
+{
+_putchar('0' + (i / 10));
+}
+_putchar('0' + (i % 10));
+i++;
+}
+i = '0';
+_putchar('\n');
+}
+
+}
