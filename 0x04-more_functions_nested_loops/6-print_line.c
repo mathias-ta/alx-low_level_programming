38e2a9faa48040c31dc7e6f17f9aff754e138c6d
mathias-tad <mathiasstadesse@gmail.com>
2021-06-24 04:37:06 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers - print 0 to 9 exept 2 & 4
+ *Return: void
+ */
+
+void print_line(int n)
+{
+int i = 0
+
+while (i < n)
+{
+
+_putchar('_');
+
+i++;
+}
+_putchar('\n');
+
+
+}
