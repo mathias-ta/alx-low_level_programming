02b96db42d7f8766a9a9a7d26371c6f47512a760
mathias-tad <mathiasstadesse@gmail.com>
2021-06-28 04:31:47 -0700

    char to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_atoi - change character into int
+ *@s: character to be changed
+ *Return: an int
+ */
+
+int _atoi(char *s)
+{
+
+int i = charToInt(s);
+
+return (i);
+
+}
