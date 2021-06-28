4fab5d6d14a1b3a19bbdf5d33bed17bbea22f505
mathias-tad <mathiasstadesse@gmail.com>
2021-06-27 23:16:01 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *swap_int - swaps values of pointer a and b
+ *@a: intiger 1
+ *@b: integer 2
+ *Return: returns void
+ */
+
+int _strlen(char *s)
+{
+
+return (s.length);
+
+}
