4e655eb517d392d5f0e9b6750dc623f24e282011
mathias-tad <mathiasstadesse@gmail.com>
2021-06-28 03:44:35 -0700

    coppy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - coppy a string
+ *@dest: storing var
+ *@src: string to be coppied
+ *Return: coppied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+return (*dest = *src);
+
+}
