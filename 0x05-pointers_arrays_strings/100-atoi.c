321ad09e2e23f82192a703d1533d967be1304876
mathias-tad <mathiasstadesse@gmail.com>
2021-06-28 10:20:48 -0700

    char to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -41,7 +41,7 @@ break;
 i++;
 s++;
 }
-if (sign % 2 != 0)
+if ((sign % 2) != 0)
 {
 return (-1);
 }
@@ -61,7 +61,7 @@ int _atoi(char *s)
 {
 
 int prod = 1;
-long int fnum = 0;
+int fnum = 0;
 int num;
 int leng = 0;
 while(*s != '\0')
