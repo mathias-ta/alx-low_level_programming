ab75488c35ba8860e4613ec9dd851b8020422f93
mathias-tad <mathiasstadesse@gmail.com>
2021-06-28 01:10:27 -0700

    Every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ *_strlen - length of a string
+ *@s: string
+ *Return: length of the string s
+ */
+
+int _strlen(char *s)
+{
+int len;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
+
+/**
  *puts2 - print every other character of a string
  *@str: string
  *Return: void
@@ -9,11 +27,11 @@
 
 void puts2(char *str)
 {
-
-while (*str != '\0')
-{
-putchar(*str);
+int i = 0;
+while (*str != '\0' && i <  _strlen(str))
+putchar(str[i]);
 str += 2;
+i++;
 }
 putchar('\n');
 
