092b92bcfcf6c5897ec5f4ae7a867b6fede23b20
mathias-tad <mathiasstadesse@gmail.com>
2021-07-02 00:53:02 -0700

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i = 0;
 int j = 0;
 
-while ((dest[i] != '\0') && (j <= (n - 1)))
+while ((src[j] != '\0') && (j <= (n - 1)))
 {
 dest[i] = src[j];
 i++;
