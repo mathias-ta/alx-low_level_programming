8066b657dbeec4cf1ac8c1f012417f8d2b830147
mathias-tad <mathiasstadesse@gmail.com>
2021-07-02 01:53:34 -0700

    String compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strcmp - copies a string
+ *@s1: string 1
+ *@s2: string 2
+ *Return: concatenated string
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+
+int i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] == s2[i])
+{
+continue;
+}
+else
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+
+
+}
