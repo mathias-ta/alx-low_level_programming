7483d3f8a57e97d717c234c5515918b57ebfc0da
mathias-tad <mathiasstadesse@gmail.com>
2021-07-02 07:10:53 -0700

    Root13 encoding

---
+++ 0x06-pointers_arrays_strings/100-root13.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *leet - change some letters with a number
+ *@s: string
+ *Return: changed string
+ */
+
+char *leet(char *s)
+{
+
+char lt[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+char LT[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+char rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z'};
+char ROT[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+int j = 0;
+int k = 0;
+
+while (s[k] != '\0')
+{
+while (j < 5)
+{
+if (s[k] == lt[j])
+{
+s[k] = rot[j];
+}
+else if (s[k] == LT[j])
+{
+s[k] = ROT[j];
+}
+else if (s[k] == rot[j])
+{
+s[k] = lt[j];
+}
+else if (s[k] == ROT[j])
+{
+s[k] = LT[j];
+}
+j++;
+}
+j = 0;
+k++;
+}
+return (s);
+
+}
