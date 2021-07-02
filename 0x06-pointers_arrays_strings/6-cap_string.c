fe10b003c5480cf0e00745ab2714cf6c0d83a32d
mathias-tad <mathiasstadesse@gmail.com>
2021-07-02 05:29:09 -0700

    Capitalize words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *cap_string - change to starting letter to uppercase
+ *@s: string
+ *Return: uppercase string
+ */
+
+char *cap_string(char *s)
+{
+
+int j = 0;
+
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] = s[0] - ' ';
+}
+j++;
+while (s[j] != '\0')
+{
+if ((s[j - 1] >= '\t' && s[j - 1] <= '?') || s[j - 1] == '{' || s[j - 1] == '}')
+{
+if (s[j] >= 'a' && s[j] <= 'z')
+{
+s[j] = s[j] - ' ';
+}
+}
+j++;
+}
+return (s);
+
+}
