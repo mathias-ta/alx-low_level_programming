abda5fad7e3c9a6059f64efa8b4f326b03042e1c
mathias-tad <mathiasstadesse@gmail.com>
2021-07-02 03:39:39 -0700

    Reversing an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *reverse_array - reveses content of an array
+ *@a: array
+ *@n: number of an array
+ *Return: void
+ */
+
+int reverse_array(int *a, int n)
+{
+
+int i = n - 1;
+int j = 0;
+int *rev_a;
+
+while (i >= 0)
+{
+rev_a[j] = a[i];
+i--;
+j++;
+}
+j = 0;
+while (j < n)
+{
+a[j] = rev_a[j];
+j++;
+}
+return (a);
+
+
+}
