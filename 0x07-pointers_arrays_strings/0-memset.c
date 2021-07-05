690ec61a758a1daf710bf8f452629dca98e839e2
mathias-tad <mathiasstadesse@gmail.com>
2021-07-05 03:27:46 -0700

    Fill memory with a byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,13 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-int i = 0;
+unsigned int i = 0;
+unsigned char *mem = s;
 
 while (i < n)
 {
-s[i] = b;
+mem[i] = b;
 i++;
 }
-return (s);
+return (mem);
 }
