22ecb2f8a350fe9df0ed9abac44a813e9c3295b1
mathias-tad <mathiasstadesse@gmail.com>
2021-07-05 03:06:19 -0700

    Fill memory with a byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_memset - fills memory with a constant byte
+ *@s: memory
+ *@b: constant byte
+ *@n: number of bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+int i = 0;
+
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
+}
