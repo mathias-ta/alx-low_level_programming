c64af1c05b09cca049ae90d9ffdb389c0abcff5c
mathias-tad <mathiasstadesse@gmail.com>
2021-07-05 04:14:22 -0700

    Find a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ *_strchr - find a char from a string
+ *@s: string
+ *@c: character to be find
+ *Return: char found
+ */
+
+char *_memcpy(char *s, char c)
+{
+
+unsigned int i = 0, j = 0;
+int counter = 0;
+char *s;
+while (s[i] != '0')
+{
+counter++;
+i++;
+}
+i= 0;
+
+while (i < counter)
+{
+if (s[i] == c)
+{
+while (i < counter)
+{
+ss[j] = s[i];
+i++;
+j++;
+}
+return (ss);
+}
+}
+
+return (NULL);
+}
