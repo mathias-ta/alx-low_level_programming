5da01a08e39cee1068209534d9acafa1246bf7bd
mathias-tad <mathiasstadesse@gmail.com>
2021-07-05 06:16:38 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#define NULL 0
+
+/**
+ *_strspn - gets length of aprifix
+ *@s: string
+ *@accept: prifix
+ *Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+int i = 0;
+int j = 0;
+int count = 0;
+
+while (s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+count++;
+}
+}
+}
+return (count);
+}
