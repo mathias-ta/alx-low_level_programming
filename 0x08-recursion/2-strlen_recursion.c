d9726b416349fb2afc7d53832afd221e18331cf5
mathias-tad <mathiasstadesse@gmail.com>
2021-07-07 06:13:16 -0700

    Count a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-int count;
-
-count++;
-if (*s != '\0')
+if (*s == '\0')
 {
-s++;
-_strlen_recursion(s);
+return (0);
 }
-return (count);
+s++;
+return (1 + _strlen_recursion(s));
 }
