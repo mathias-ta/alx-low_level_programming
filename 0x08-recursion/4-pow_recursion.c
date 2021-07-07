61c9bcdfc795b9b971d2f9f86471e46b686cd2bd
mathias-tad <mathiasstadesse@gmail.com>
2021-07-07 06:48:06 -0700

    x raise to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion - power of a number
+ *@x: number
+ *@y: power
+ *Return: x raise to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (x < 0)
+{
+return (-1);
+}
+if (x >= 0 && y == 0)
+{
+return (1);
+}
+return (x * _pow_recursin(x, y - 1));
+}
