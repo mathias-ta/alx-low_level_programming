597481c1dbab0d4be5ea0cc0c97ae863746f6030
mathias-tad <mathiasstadesse@gmail.com>
2021-07-07 04:04:50 -0700

    Reverse print string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - prints reverse of a string
+ *@s: string to be printed
+ *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+int i = 0;
+if (*s != '\0')
+{
+ss[i] = *s;
+i++;
+s++;
+_print_rev_recursion(s);
+}
+else
+{
+while (i >= 0)
+{
+_putchar(*s);
+i--;
+}
+_putchar('\n');
+}
+}
