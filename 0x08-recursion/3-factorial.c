72676e2dc524692770a98d5664436e3a93f393bd
mathias-tad <mathiasstadesse@gmail.com>
2021-07-07 06:24:13 -0700

    Factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *factorial - factorial of a number
+ *@n: number
+ *Return: factorial of n
+ */
+
+int factorial(int n)
+{
+if (n = 0)
+{
+return (1);
+}
+return (n * _strlen_recursion(n--));
+}
