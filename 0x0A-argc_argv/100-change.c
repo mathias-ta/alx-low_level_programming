f6b2779d0db57315c4667343e4ab81c48e8d5511
mathias-tad <mathiasstadesse@gmail.com>
2021-07-12 11:33:23 -0700

    Change to cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ *main - print sum if all arguments are numbers
+ *@argc: argument counter
+ *@argv: pointer to the array of argument
+ *Return: 0 for succes 1 if an arg is not a number
+ */
+
+int main(int argc, char *argv[])
+{
+
+int cents[] = {25, 10, 5, 2};
+int sum = 0;
+int rem = 0;
+int que;
+int i = 0;
+
+if (atoi(argv[1]) < 0)
+{
+printf("0\n");
+return (0);
+}
+if (argc == 1)
+{
+que = atoi(argv[1]) / 25;
+rem = atoi(argv[1]) % 25;
+sum += que;
+while (rem != 0)
+{
+i++;
+rem = que % cents[i];
+que = rem / cents[i];
+sum += que;
+}
+printf("%d\n", sum);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
