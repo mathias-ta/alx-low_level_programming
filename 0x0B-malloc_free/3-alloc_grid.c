db88e2d5acca3d0eeca940231482bbf891017d8f
mathias-tad <mathiasstadesse@gmail.com>
2021-07-13 09:39:26 -0700

    Allocate a 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - concatinate two strings
+ *@width: width of the grid
+ *@height: height of the grid
+ *Return: allocated grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i = 0, j = 0, len1 = 0, len2 = 0;
+char **p;
+if (width == NULL || height == NULL)
+{
+return (NULL);
+}
+p = malloc(sizeof(int) * (width) * (length) +1);
+if (p == NULL)
+{
+return (NULL);
+}
+while (i < width)
+{
+while (j < height)
+{
+p[i][j] = 0;
+j++;
+}
+j = 0;
+i++;
+}
+
+}
