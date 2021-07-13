af5c7afc7cee535618026796e0b4a6f2dd52353b
mathias-tad <mathiasstadesse@gmail.com>
2021-07-13 04:20:31 -0700

    Create array with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define NULL 0
 
 /**
  *create_array - creates array with the specified size and assign a value
  *@size: size of the array to be created
  *@c: initializing value
+ *Return - array created
  */
 
 char *create_array(unsigned int size, char c)
 {
 unsigned int i = 0;
+if (size == 0)
+{
+return (NULL);
+}
 char *p = malloc(size);
 while (i < size)
 {
