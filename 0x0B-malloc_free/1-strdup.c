9e588c6d05ae5348b057d8e22d0fb40bfda149af
mathias-tad <mathiasstadesse@gmail.com>
2021-07-13 05:38:17 -0700

    strdup function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,16 +13,16 @@ char *_strdup(char *str)
 unsigned int i = 0;
 int  len = 0;
 char *p;
+if (str == NULL)
+{
+return (NULL);
+}
 while (*str != '\0')
 {
 len++;
 str++;
 }
 p = malloc(sizeof(char) * len + 1);
-if (str == NULL)
-{
-return (NULL);
-}
 while (*str != '\0')
 {
 *(p + i) = *(str + i);
