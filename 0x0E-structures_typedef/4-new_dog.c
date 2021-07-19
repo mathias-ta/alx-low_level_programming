d32d27c91544a550fcb05898234c63e117226d9a
mathias-tad <mathiasstadesse@gmail.com>
2021-07-19 06:41:10 -0700

    Create new strunct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *new_dog - creates a new struct dog
+ *@name: member
+ *@age: member
+ *@owner: member
+ *Return: void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+struct dog *d;
+if (d == NULL) /*Validate d*/
+{
+return;
+}
+
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+
+return d;
+}
