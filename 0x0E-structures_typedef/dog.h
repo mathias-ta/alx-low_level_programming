ba15eb2e8e17117bbe1713b031add876339b1332
mathias-tad <mathiasstadesse@gmail.com>
2021-07-19 03:26:25 -0700

    Define a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ *dog - structure defines dog with name, age and owner
+ *@name: member
+ *@age: member
+ *@owner: member
+ */
+
 struct dog {
   char *name;
   float age;
