a46927765491e16a60d87abff95d28560ab98610
mathias-tad <mathiasstadesse@gmail.com>
2021-07-22 13:31:05 -0700

    Print all datatypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef _VARDIAC_FUNCTIONS_H_
 #define _VARDIAC_FUNCTIONS_H_
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separetor, const unsigned int n, ...);
@@ -8,5 +10,13 @@ void print_numbers(const char *separetor, const unsigned int n, ...);
 int _putchar(char c);
 
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+typedef struct ch
+{
+char cc;
+void (*f)(va_list);
+} datatype;
 
 #endif
