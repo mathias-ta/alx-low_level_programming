7200fa2e0fb9e1c8a3302dbc25ad7cb2433f0c4a
mathias-tad <mathiasstadesse@gmail.com>
2021-07-22 14:38:30 -0700

    Print in asembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,17 @@
-	section .data
-	text db "Hello, Holberton",10
-	section .text
-	global _start
+
+section .text
+
 _start:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
+	mov rsi, msg
+	mov rdx, msglen
 	syscall
 
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+section .rodata
+msg:	db "Hello, Holberton", 10
+msglen:	equ $ - msg
