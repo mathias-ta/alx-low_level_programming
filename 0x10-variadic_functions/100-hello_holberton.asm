10dabd4320f8c81da8dcd7c823dc25f31d5e2014
mathias-tad <mathiasstadesse@gmail.com>
2021-07-23 06:21:38 -0700

    Update the asembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,25 @@
+section .data
+	str db 0xa, "Hello, Holberton", 0xa, 0xa, 0
 
 section .text
+	global _start
 
 _start:
+	mov rdi, str
+	xor rcx, rcx
+	not rcx
+	xor al,al
+	cld
+	repnz scasb
+	not rcx
+	dec rcx
+	mov rdx, rcx
+
+	mov rsi, str
 	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, msglen
+	mov rdi, rax
 	syscall
 
-	mov rax, 60
-	mov rdi, 0
+	xor rdi,rdi
+	mov rax, 0x3c
 	syscall
-
-section .rodata
-msg:	db "Hello, Holberton", 10
-msglen:	equ $ - msg
