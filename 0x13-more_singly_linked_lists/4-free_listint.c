df564bc680aae988e2f6160bc9ab5d5299f2a6d0
mathias-tad <mathiasstadesse@gmail.com>
2021-08-04 10:27:49 -0700

    Free linked list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,13 +9,14 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	if (head == NULL) /* account for no linked list */
+	if (head == NULL)
 		return;
 
-	while (head != NULL) /* have ptr keep track of head node and free */
+	while (head != NULL)
 	{
 		ptr = head;
-		head = (*head).next; /* move to next node while ptr frees prior */
+		head = (*head).next;
 		free(ptr);
 	}
 
+}
